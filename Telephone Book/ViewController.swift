a37b5a831f1ff878f5df8d444647efb6c239d826
User <user@macbook-pro-mac-2.local>
2018-08-16 14:38:52 +0300

    Initial Commit

---
+++ Telephone Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telephone Book
+//
+//  Created by User on 16.08.2018.
+//  Copyright © 2018 e-legion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
